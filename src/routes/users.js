118283559237bca18c4ce07c4fa80fcb7e7fc523
Matias <matiascejas8@gmail.com>
2019-03-08 08:30:02 -0300

    se crearon las vistas basicas

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/users/signin', (req, res) => {
-  res.send('Ingresando a la aplicación')
+  res.render('users/signin')
 })
 
 router.get('/users/signup', (req, res) => {
-  res.send('Formulario de autenticación')
+  res.render('users/signup')
 })
 
 module.exports = router
