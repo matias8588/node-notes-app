89d66e87db1617aeb8d06fa3f8a0d0dc2e662b87
Matias <matiascejas8@gmail.com>
2019-03-10 23:05:44 -0300

    preparacion de app con heroku

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/notesApp', {
+mongoose.connect('mongodb+srv://matias:matias131205@cluster0-bhpbz.mongodb.net/test?retryWrites=true', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
